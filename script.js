60cc59de532b338c82bbf45c8eb0985ee52cd5f8
gleicekelly13 <gleice.kelly22@hotmail.com>
2024-08-18 10:43:25 -0300

    Início do script, selecioanndo todos os elementos da célula

---
+++ script.js
@@ -0,0 +1,4 @@
+/* Seleção de todos os elementos da célula */
+const cells = document.querySelectorAll('[data-cell]');
+const tabuleiro = document.getElementById('tabuleiro');
+const restartButton = document.getElementById('restartButton');
