8001889b79fd599a0c2d46aab6eb990bd7aba85f
gleicekelly13 <gleice.kelly22@hotmail.com>
2024-08-19 10:32:08 -0300

    Criação da constante que define as combinações vencedoras

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ const restartButton = document.getElementById('restartButton');
 /* Definição de constantes para as classes x e o. */
 const x_class = 'x';
 const o_class = 'o';
+
+/* Definição das combinações vencedoras.  */
+const winning_combinattions = [ 
+    [0, 1, 2],  // representam as posições das células que formam uma linha vencedora.
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7], 
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
